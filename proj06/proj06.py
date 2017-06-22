4bd1d929b7b3ec9d33492811310581c6b090a46b
Natalie-N <natalie.f.nasser@gmail.com>
2017-06-22 15:39:59 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -46,3 +46,90 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+#def word1():
+
+
+
+
+def hangman():
+    print "Let's play hangman!"
+    guesses=6
+    userword = raw_input("Do you want to choose a word for your opponent to guess?")
+    if userword == "Yes" or userword == "yes" or userword == "yup" or userword == "Yup" or userword == "YUP" or userword == "Hai" or userword == "definetly" or userword == "of course!" or userword == "TOTALLY" or userword == "Yes" or userword == "YES" or userword == "yeah" or userword == "Yeah" or userword == "YEAH" or userword == "Yep" or userword == "yep" or userword == "YEP" or userword == "totally" or userword == "sure" or userword == "Totally" or userword == "sure" or userword == "Sure" or userword == "maybe" or userword == "Maybe" or userword == " yes" or userword == " yeah" or userword == " Yes" or userword == " y" or userword == "y" or userword == "Y" or userword == " yeah" or userword == " Y":
+        word = raw_input("Enter a word: ")
+        x = raw_input("Enter * to show you're ready")
+
+        # theList = []
+        # for letter in word:
+        #     theList.append(letter)
+        #     x = 0
+        #
+        #     for LETTER in theList:
+        #         theList[x] = "*"
+        #         x = x + 1
+
+
+    else:
+        word = choose_word(wordlist)
+        theList = []
+        for letter in word:
+            theList.append(letter)
+    print "You will recieve 6 wrong guesses before I name you a failure."
+    length = len(word)
+    print word
+    print "The word you are trying to guess happens to be", length, "letters long."
+
+    blanklist = []
+    for letter in word:
+        blanklist.append('_')
+        guesslist = []
+    while guesses>0:
+        print " ".join(blanklist)
+        g = raw_input ("Guess a letter ")
+        a = -1
+        b = -1
+        c = True
+        if g == word:
+            print "You got it correct."
+            break
+            newgame = raw_input("Do you want to play again? *evil face*")
+            while newgame == "Yes" or newgame == "yes" or newgame == "yup" or newgame == "Yup" or newgame == "YUP" or newgame == "Hai" or newgame == "definetly" or newgame == "of course!" or newgame == "TOTALLY" or newgame == "Yes" or newgame == "YES" or newgame == "yeah" or newgame == "Yeah" or newgame == "YEAH" or newgame == "Yep" or newgame == "yep" or newgame == "YEP" or newgame == "totally" or newgame == "sure" or newgame == "Totally" or newgame == "sure" or newgame == "Sure" or newgame == "maybe" or newgame == "Maybe" or newgame == " yes" or newgame == " yeah" or newgame == " Yes" or newgame == " y" or newgame == "y" or newgame == "Y" or newgame == " yeah" or newgame == " Y":
+                hangman()
+        for letters in theList:
+                b=b+1
+                a=a+1
+                #print "a=",a,"and b=",b
+                for letter in g:
+                    if theList[a]== g:
+                        #print "list",a,"=",g
+                        print "You got a letter right."
+                        blanklist[b]=g
+                        c = False
+                        #b=b+1
+                        #a=a+1
+                    elif b >= length-1 and c==True:
+                        #print "b=",b,"and length=",length
+                        guesses = guesses - 1
+                        #print "guesses=",guesses
+                        print "You got the letter wrong. You failed, but I'll give you",guesses,"guesses more before I truly name you a failure."
+                        guesslist.append(g)
+                        print "These are your incorrect guesses:"," ".join(guesslist)
+                        if guesses == 0:
+                            print "GAME OVER. I NAME YOU A FAILURE."
+
+
+                    #else:
+                        #a=a+1
+                        #b=b+1
+
+
+hangman()
+
+
+newgame = raw_input("Do you want to play again? *evil face*")
+while newgame == "Yes" or newgame=="yes" or newgame == "yup" or newgame == "Yup" or newgame == "YUP" or newgame == "Hai" or newgame == "definetly" or newgame == "of course!" or newgame == "TOTALLY" or newgame=="Yes" or newgame=="YES" or newgame=="yeah" or newgame=="Yeah" or newgame=="YEAH" or newgame=="Yep" or newgame=="yep" or newgame=="YEP" or newgame=="totally" or newgame=="sure" or newgame=="Totally" or newgame=="sure" or newgame=="Sure" or newgame=="maybe" or newgame=="Maybe" or newgame==" yes" or newgame==" yeah" or newgame==" Yes" or newgame==" y" or newgame=="y" or newgame=="Y" or newgame==" yeah" or newgame==" Y":
+
+    hangman()
+
+
+
