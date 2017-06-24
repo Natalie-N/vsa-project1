77f75b0c868e6eaec436cffe11170b593d1fe517
Natalie-N <natalie.f.nasser@gmail.com>
2017-06-24 11:53:52 -0500

    Add files via upload

--- proj07/proj07.py
+++ proj07/proj07.py
@@ -1,4 +1,4 @@
-# Name:
+# Name: bonnie and natalie
 # Date:
 
 # proj07: Word Game
@@ -22,10 +22,11 @@ SCRABBLE_LETTER_VALUES = {
 
 WORDLIST_FILENAME = "words.txt"
 
+
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -39,6 +40,7 @@ def load_words():
     print "  ", len(wordlist), "words loaded."
     return wordlist
 
+
 def get_frequency_dict(sequence):
     """
     Returns a dictionary where the keys are elements of the sequence
@@ -51,9 +53,9 @@ def get_frequency_dict(sequence):
     # freqs: dictionary (element_type -> int)
     freq = {}
     for x in sequence:
-        freq[x] = freq.get(x,0) + 1
+        freq[x] = freq.get(x, 0) + 1
     return freq
-	
+
 
 # (end of helper code)
 # -----------------------------------
@@ -62,7 +64,52 @@ def get_frequency_dict(sequence):
 # Problem #1: Scoring a word
 #
 def get_word_score(word, n):
-    """
+
+    sum = 0
+    for letters in word:
+
+        letter_score = SCRABBLE_LETTER_VALUES[letters]
+        sum = sum + letter_score
+
+    score = sum * len(word)
+
+    if len(word) == n:
+        score = score + 50
+
+    print "score after multiplying is", score
+    return score
+
+
+# take every letter in their wordOK
+# check what every letter is worth
+# add up what all the letters are worth
+# multiply that by length(word)
+
+
+# wordinput = raw_input ("Enter your word!")
+# score = 0
+# letterlist=[]
+# for letters in word:
+#     score = score + SCRABBLE_LETTER_VALUES[letters]
+#     letterlist.append(letters)
+# wordlength = len(letterlist)
+# wordscore = score*wordlength
+# print score, " is the score before mutiplying"
+# print wordscore, "is after multiplying"
+# print letterlist
+
+# stringlist.append(word)
+# for letters in letterlist:
+#     if letters in letterlist:
+#         print "if state worked"
+# index=0
+# for key in SCRABBLE_LETTER_VALUES:
+#     if key in letterlist:
+#         comparedlist = letterlist.copy()
+#         print comparedlist
+# SCRABBLE_LETTER_VALUES
+
+"""
     Returns the score for a word. Assumes the word is a
     valid word.
 
@@ -76,8 +123,10 @@ def get_word_score(word, n):
     word: string (lowercase letters)
     returns: int >= 0
     """
-    # TO DO...
-    
+
+
+# TO DO...
+
 #
 # Make sure you understand how this function works and what it does!
 #
@@ -95,8 +144,9 @@ def display_hand(hand):
     """
     for letter in hand.keys():
         for j in range(hand[letter]):
-             print letter,              # print all on the same line
-    print                               # print an empty line
+            print letter,  # print all on the same line
+    print  # print an empty line
+
 
 #
 # Make sure you understand how this function works and what it does!
@@ -113,54 +163,140 @@ def deal_hand(n):
     n: int >= 0
     returns: dictionary (string -> int)
     """
-    hand={}
+    hand = {}
     num_vowels = n / 3
-    
+
     for i in range(num_vowels):
-        x = VOWELS[random.randrange(0,len(VOWELS))]
+        x = VOWELS[random.randrange(0, len(VOWELS))]
         hand[x] = hand.get(x, 0) + 1
-        
-    for i in range(num_vowels, n):    
-        x = CONSONANTS[random.randrange(0,len(CONSONANTS))]
+
+    for i in range(num_vowels, n):
+        x = CONSONANTS[random.randrange(0, len(CONSONANTS))]
         hand[x] = hand.get(x, 0) + 1
-        
+
     return hand
+
 
 #
 # Problem #2: Update a hand by removing letters
-#
+# hand down below is ony a placeholder. pretend "hand" actually does represent something
+
+
 def update_hand(hand, word):
-    """
-    Assumes that 'hand' has all the letters in word.
-	In other words, this assumes that however many times
-	a letter appears in 'word', 'hand' has at least as
-	many of that letter in it. 
+    new_hand = hand.copy()
 
-    Updates the hand: uses up the letters in the given word
-    and returns the new hand, without those letters in it.
+    leftoverletters = {}
+    letterlist2 = []
+    for letters in word:
+        letterlist2.append(letters)
+    print hand
 
-    Has no side effects: does not modify hand.
+    for letters in word:
+        # if letters in hand:
+        #     print "you already used these dummy"
+        if letters in hand:
 
-    word: string
-    hand: dictionary (string -> int)    
-    returns: dictionary (string -> int)
-    """
-    # TO DO ...
+
+            new_hand[letters] = new_hand[letters] - 1
+            print new_hand[letters]
+
+    print new_hand[letters]
+    print new_hand, 'this is what you have left'
+
+    return new_hand
+#update_hand(deal_hand(10), 'hello')
+
+#update_hand(deal_hand(10), 'hello')
+
+
+#     for letters in hand:
+#         if letters not in letterlist2:
+#             get.frequency_dict(letters)
+#     print leftoverletters, "these are the letters you ave left and can still use"
+#
+#
+#
+#     return new_hand
+#
+#
+#
+#
+#     leftoverletters=[]
+#     letterlist2 = []
+#     for letters in word:
+#         letterlist2.append(letters)
+#
+#     for letters in hand:
+#         if letters not in letterlist2:
+#             leftoverletters.append(letters)
+#
+#     print leftoverletters, "these are the letters you have left and can still use"
+#     return leftoverletters
+# update_hand(deal_hand(HAND_SIZE), "hello")
+
+
+
+
+# letterlist has the words entered by player
+# """
+# Assumes that 'hand' has all the letters in word.
+# In other words, this assumes that however many times
+# a letter appears in 'word', 'hand' has at least as
+# many of that letter in it.
+#
+# Updates the hand: uses up the letters in the given word
+# and returns the new hand, without those letters in it.
+#
+# Has no side effects: does not modify hand.
+#
+# word: string
+# hand: dictionary (string -> int)
+# returns: dictionary (string -> int)
+# """
+# TO DO ...
 
 #
 # Problem #3: Test word validity
 #
 def is_valid_word(word, hand, word_list):
-    """
-    Returns True if word is in the word_list and is entirely
-    composed of letters in the hand. Otherwise, returns False.
-    Does not mutate hand or word_list.
-    
-    word: string
-    hand: dictionary (string -> int)
-    word_list: list of lowercase strings
-    """
-    # TO DO...
+    letterlist3 = []
+    for letters in word:
+        letterlist3.append(letters)
+    number = 0
+    number2 = 0
+
+    if word not in word_list:
+        print "invalid word. not in word list."
+    if word in word_list:
+        print "valid word. in word list. good job"
+
+    for letters in word:
+        if letters in letterlist3:
+            number2 = number2 + 1
+
+        if letters in hand:
+            number = number + 1  # letter has not been used before
+            print "good one!"
+
+
+        elif letters not in hand:
+            print "terrible! you used a letter you dont have. >:("
+            return False
+    if number < number2:
+        return False
+
+    return True
+
+
+# is_valid_word("hello",deal_hand(HAND_SIZE), load_words())
+
+
+
+
+
+
+
+# TO DO...
 
 def calculate_handlen(hand):
     handlen = 0
@@ -168,16 +304,16 @@ def calculate_handlen(hand):
         handlen += v
     return handlen
 
+
 #
 # Problem #4: Playing a hand
 #
 def play_hand(hand, word_list):
-
     """
     Allows the user to play the given hand, as follows:
 
     * The hand is displayed.
-    
+
     * The user may input a word.
 
     * An invalid word is rejected, and a message is displayed asking
@@ -197,14 +333,15 @@ def play_hand(hand, word_list):
 
       hand: dictionary (string -> int)
       word_list: list of lowercase strings
-      
+
     """
     # TO DO ...
+
 
 #
 # Problem #5: Playing a game
 # Make sure you understand how this code works!
-# 
+#
 def play_game(word_list):
     """
     Allow the user to play an arbitrary number of hands.
@@ -222,9 +359,38 @@ def play_game(word_list):
     """
     # TO DO...
 
+
 #
 # Build data structures used for entire session and play game
 #
 if __name__ == '__main__':
     word_list = load_words()
     play_game(word_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
